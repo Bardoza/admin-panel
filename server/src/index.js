a4a2faf8e60062d51d4a21043b85103946f786d6
Eduardo Obregón <eduardo.obz@gmail.com>
2017-07-02 02:19:04 -0500

    add express js and babel configuration to package.json

--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,19 @@
-const test = 'Testing babel'
-console.log(`test!:   ${test}`)
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+// test express
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+app.use((err, req, res, next) => {
+  console.log('unhandled application error: ', err);
+  res.status(500).send(err);
+});
